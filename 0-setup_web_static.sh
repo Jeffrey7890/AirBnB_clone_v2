e43a4480af2fa3b65b86e4379380e95bf28096fd
jeffrey7890 <d3v3loperpy@gmail.com>
2024-07-13 15:14:25 -0700

    new config again

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,22 +3,9 @@
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install nginx
-DIRS=("/data/" "/data/web_static/" "/data/web_static/releases/" "/data/web_static/shared/" "/data/web_static/releases/test/")
-
-for dir in "${DIRS[@]}"; do
-        if [ ! -d "$dir" ]; then
-                mkdir "$dir"
-        fi
-done
-
-echo "<html>
-        <head>
-        </head>
-        <body>
-                Holberton School
-        </body>
-</html>" > /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+chown -hR ubuntu:ubuntu /data/
 sed -i "/server_name _;/a \ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}" /etc/nginx/sites-enabled/default
-systemctl restart nginx
+service nginx start
