8cf8374c75baf5d9504ea8825d9c57af0fe350c1
jeffrey7890 <d3v3loperpy@gmail.com>
2024-07-13 14:44:15 -0700

    setup webstatic

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,40 +1,23 @@
 #!/usr/bin/env bash
-# A Bash script that sets up your web servers for the deployment of web_static
+# setup a static web site
+apt update
+apt-get install -y nginx
+DIRS=("/data/" "/data/web_static/" "/data/web_static/releases/" "/data/web_static/shared/" "/data/web_static/releases/test/")
 
-# install nginx
-if ! (nginx -v)
-then
-    sudo apt-get update -y
-    sudo apt-get install -y nginx
-fi
+for dir in "${DIRS[@]}"; do
+        if [ ! -d "$dir" ]; then
+                mkdir "$dir"
+        fi
+done
 
-# create the desired directories
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-
-# create an html index page containing the content of the idx_cont variable
-idx_cont="<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>"
-
-echo "$idx_cont" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-
-# pcreate a symbolic link to current to the test directory
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# change ownership of the /data/ folder to the ubuntu user AND group
-sudo chown --recursive ubuntu:ubuntu /data
-
-# configure nginx to serve the index of /data/web_static/current when hbnb_static/ dir is queried
-sedStr="\\
-\tlocation /hbnb_static {\\
-\t\t alias /data/web_static/current/;\\
-\t}"
-sudo sed -i '/server_name _;/a '"$sedStr"'' /etc/nginx/sites-enabled/default
-
-# restart nginx
-sudo service nginx restart
+echo "<html>
+        <head>
+        </head>
+        <body>
+                Holberton School
+        </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i "/server_name _;/a \ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}" /etc/nginx/sites-enabled/default
+systemctl restart nginx
