f83a01a7d7c2ba811fb83d171a911702aad09e55
Jeffrey7890 <d3v3loperpy@gmail.com>
2024-07-14 15:45:59 -0700

    deploy static files

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,22 @@
 #!/usr/bin/python3
-"""
-This module contains a function used to deploy the archived html scripts to
-out server
-"""
-
-import os
-from fabric.api import *
-from pathlib import Path
-
-
-env.hosts = ['3.90.82.95', '54.157.156.207']
+from fabric.api import run, env
+env.hosts = ['54.158.79.193', '52.204.95.113']
 
 
 def do_deploy(archive_path):
-    """
-    Function to respectively deploy the static to their respective locations
-    on server
-    """
-    pathObj = Path(archive_path)
-    if not pathObj.exists:
-        return False
-
-    tmpDest = f"/tmp/{pathObj.name}"
-    destDir = f"/data/web_static/releases/{pathObj.stem}/"
-    put(archive_path, tmpDest)
-    exec_1 = sudo(f"mkdir -p {destDir}")
-    exec_2 = sudo(f"tar -xzf {tmpDest} -C {destDir}")
-    exec_3 = sudo(f"rm {tmpDest}")
-    exec_4 = sudo(f"mv {destDir}/web_static/* {destDir}")
-    exec_5 = sudo(f"rm -rf {destDir}/web_static")
-    exec_6 = sudo(f"rm -rf /data/web_static/current")
-    exec_7 = sudo(f"ln -s {destDir} /data/web_static/current")
-    exec_list = [exec_1, exec_2, exec_3, exec_4, exec_5, exec_6, exec_7]
-    return True if all([obj.succeeded for obj in exec_list]) else False
+    """ deploys my websites """
+    try:
+        archive_name_tgz = archive_path.split('/')[1]
+        archive_name = archive_name_tgz.split('.')[0]
+        put(archive_path, f'/tmp/{archive_name_tgz}')
+        run(f'mkdir -p /data/web_static/releases/{archive_name}')
+        run(f'tar -xzf /tmp/{archive_name_tgz} -C /data/web_static/releases/{archive_name}/')
+        run(f'rm /tmp/{archive_name_tgz}')
+        run(f'mv /data/web_static/releases/{archive_name}/web_static/* /data/web_static/releases/{archive_name}/')
+        run(f'rm -rf /data/web_static/releases/{archive_name}/web_static')
+        run('rm -f /data/web_static/current')
+        run(f'ln -s /data/web_static/releases/{archive_name}/ /data/web_static/current')
+        print('New version deployed!')
+        return (True)
+    except Exception as e:
+        return (False)
