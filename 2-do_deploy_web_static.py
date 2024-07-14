901c10a5ae722557d1310fe580140267ac5ef7d8
Jeffrey7890 <d3v3loperpy@gmail.com>
2024-07-14 16:12:23 -0700

    deploy web static again

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,15 +8,14 @@ def do_deploy(archive_path):
     try:
         archive_name_tgz = archive_path.split('/')[1]
         archive_name = archive_name_tgz.split('.')[0]
-        path_to_archive_name = f"/data/web_static/releases/{archive_name}"
         put(archive_path, f'/tmp/{archive_name_tgz}')
-        run(f'mkdir -p {path_to_archive_name')
-        run(f'tar -xzf /tmp/{archive_name_tgz} -C {path_to_archive_name}')
+        run(f'mkdir -p /data/web_static/releases/{archive_name}')
+        run(f'tar -xzf /tmp/{archive_name_tgz} -C /data/web_static/releases/{archive_name}/')
         run(f'rm /tmp/{archive_name_tgz}')
-        run(f'mv {path_to_archive_name}/web_static/* {path_to_archive_name}')
-        run(f'rm -rf {path_to_archive_name}/web_static')
+        run(f'mv /data/web_static/releases/{archive_name}/web_static/* /data/web_static/releases/{archive_name}/')
+        run(f'rm -rf /data/web_static/releases/{archive_name}/web_static')
         run('rm -f /data/web_static/current')
-        run(f'ln -s {path_to_archive_name}/ /data/web_static/current')
+        run(f'ln -s /data/web_static/releases/{archive_name}/ /data/web_static/current')
         print('New version deployed!')
         return (True)
     except Exception as e:
